9c1051a301864570ae0444974dc8b5084dc05d9d
HARSH RAJ <122086619+Harshraj843112@users.noreply.github.com>
2025-02-12 17:39:02 +0530

    Update index.js
    forth changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write("Hellow bachho kaise hai app sabhhi aap log thim hi hanoge mai aasha karta hu "); //write a response to the client
+  res.write("Hellow bachho kaise hai app sabhhi aap log thim hi hanoge mai aasha karta hu ki aaplog thik hone am i write or not "); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
